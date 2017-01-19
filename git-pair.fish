0fca777f1d1cfe73a6d7a79dd897e28e0f9b43c1
Sahil Bajaj <sahil29@gmail.com>
2017-01-19 23:18:55 +0800

    Add top level command for setting pairs

--- git-pair.fish
+++ git-pair.fish
@@ -1,11 +1,7 @@
 #!/usr/bin/env fish
+set pairs_config_path ./.git-pairables.yaml
 
-# for i in (expr (count $argv) - 1)
-# 	echo $argv[$i]
-# end
-
-if [ $argv = 'add' ]
-	# vim pairs.yaml
+if [ $argv[1] = 'add' ]
 	echo Nickname:
 	read nickname
 	echo Name:
@@ -13,22 +9,25 @@ if [ $argv = 'add' ]
 	echo Email:
 	read email
 
-	echo "- nickname: $nickname" >> pairs.yaml
-	echo "  name: $name" >> pairs.yaml
-	echo "  email: $email" >> pairs.yaml
+	echo "- nickname: $nickname" >> $pairs_config_path
+	echo "  name: $name" >> $pairs_config_path
+	echo "  email: $email" >> $pairs_config_path
 
 	exit 0
 end
 
-grep "nickname: $argv[1]" pairs.yaml > /dev/null; or exit 1
-grep "nickname: $argv[2]" pairs.yaml > /dev/null; or exit 1
+if [ $argv[1] = 'set' ]
+	grep "nickname: $argv[2]" $pairs_config_path > /dev/null; or exit 1
+	grep "nickname: $argv[3]" $pairs_config_path > /dev/null; or exit 1
 
-set first (cat pairs.yaml | grep -A2 "nickname: $argv[1]" | cut -d':' -f2 | sed s/^\ //)
-set second (cat pairs.yaml | grep -A2 "nickname: $argv[2]" | cut -d':' -f2 | sed s/^\ //)
+	set first (cat $pairs_config_path | grep -A2 "nickname: $argv[2]" | cut -d':' -f2 | sed s/^\ //)
+	set second (cat $pairs_config_path | grep -A2 "nickname: $argv[3]" | cut -d':' -f2 | sed s/^\ //)
 
-git config user.name (echo $first[2] and $second[2])
-git config user.email (echo (echo $first[3] | cut -d'@' -f1)+$second[3])
+	git config user.name (echo $first[2] and $second[2])
+	git config user.email (echo (echo $first[3] | cut -d'@' -f1)+$second[3])
 
-set_color -u bryellow; echo gitconfig updated; set_color normal
-echo Name: (git config --get user.name)
-echo Email: (git config --get user.email)
+	set_color -u bryellow; echo gitconfig updated; set_color normal
+	echo Name: (git config --get user.name)
+	echo Email: (git config --get user.email)
+end
+
